525e85fe674fada28ba932f1efa5fc37a5caadb8
Eric Peterson <peterson.eric.c@gmail.com>
2021-01-27 21:17:14 -0800

    add operations to sync-rpc comparable to with-replies

--- src/rpc.lisp
+++ src/rpc.lisp
@@ -19,11 +19,15 @@
            (send-message ,reply-channel (make-message-rpc-done :result ,return-value)))))))
 
 (defmacro sync-rpc (message
-                    (result-place-or-list destination &key returned?)
+                    (result-place-or-list destination
+                     &key
+                       returned?
+                       (message-type 'message-rpc-done)
+                       (message-unpacker 'message-rpc-done-result))
                     &body body)
   "Performs a synchronized RPC call.  Only allowed inside the body of DEFINE-PROCESS-UPKEEP.
 
-Sends `MESSAGE' to `DESTINATION', waits for a reply, and unpacks the reply into `RESULT-PLACE-OR-LIST'.
+Sends `MESSAGE' to `DESTINATION', waits for a reply (of type `MESSAGE-TYPE'), and unpacks the reply (using `MESSAGE-UNPACKER') into `RESULT-PLACE-OR-LIST'.
 
 If `RETURNED?' is supplied and this call generates a `MESSAGE-RTS' reply, then `RETURNED?' will be flagged and control resumes.  Otherwise, controlled is interrupted by an error."
   (multiple-value-bind (body decls) (a:parse-body body)
@@ -38,16 +42,16 @@ If `RETURNED?' is supplied and this call generates a `MESSAGE-RTS' reply, then `
            ,@(unless (null returned?)
                `((message-RTS
                   (setf ,returned? t))))
-           (message-rpc-done
+           (,message-type
             ,(etypecase result-place-or-list
                (symbol
-                `(let ((,result-place-or-list (message-rpc-done-result ,message-place)))
+                `(let ((,result-place-or-list (,message-unpacker ,message-place)))
                    ,@decls
                    (declare (ignorable ,result-place-or-list))
                    (unregister ,listen-channel)
                    ,@body))
                (list
-                `(destructuring-bind ,result-place-or-list (message-rpc-done-result ,message-place)
+                `(destructuring-bind ,result-place-or-list (,message-unpacker ,message-place)
                    ,@decls
                    (unregister ,listen-channel)
                    ,@body)))))))))
